68fd7e2bfe0c76333e4c788ed719d9abcfa69122
CJUNIOR98 <carlosjuniorm360@gmail.com>
2022-06-25 16:45:13 -0500

    Se hacen arreglos en el pryoecto

--- src/components/QuoteBox.jsx
+++ src/components/QuoteBox.jsx
@@ -3,7 +3,24 @@ import React from 'react';
 import quotes from '../quotes.json'
 import image from '../images/quotation.png'
 
-const colors = [ "#06AED5", "#086788", "DD1C1A", "390099", "FF0054", "FF5400", "F19C79", "710627", "DE0D92", "320E3B", "4C2A85", "BCEDF6", "F7C7DB", "C86FC9", "003049", "4C2A85"];
+const colors = [ 
+"#06AED5", 
+"#086788", 
+"#DD1C1A", 
+"#390099", 
+"#FF0054", 
+"#FF5400", 
+"#F19C79", 
+"#710627", 
+"#DE0D92", 
+"#320E3B", 
+"#4C2A85", 
+"#BCEDF6",
+"#F7C7DB", 
+"#C86FC9", 
+"#003049", 
+"#4C2A85"
+];
 
 
 const QuoteBox = () => {
